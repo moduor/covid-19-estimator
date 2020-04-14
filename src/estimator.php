631e72ef5a0ef56e56951fc7624f3396f3d1fdaa
Maurice Oduor <maurice.oduor@gmail.com>
2020-04-14 16:01:31 +0300

    More changes to the estimator

--- src/estimator.php
+++ src/estimator.php
@@ -15,6 +15,23 @@
             totalHospitalBeds: 1380614
         }
     */
+    function get_num_days($periodType, $timeToElapse){
+        $days = 0;
+        switch($periodType){
+            case 'days':
+                $days = $timeToElapse;
+                break;
+            case 'weeks':
+                $days = (int)($timeToElapse * 7);
+                break;
+            case 'months':
+                $days = (int)($timeToElapse * 30);
+                break;
+            default:
+                $days = $timeToElapse;
+        }
+        return $days;
+    }
 
     function covid19ImpactEstimator($data)
     {
@@ -22,14 +39,25 @@
         $severeImpact = array();
 
         $receivedData = json_decode($data, true);
+
+        $averageInconme = $receivedData['region']['avgDailyIncomeInUSD']; // Average daily income
+        $averageIncomePop = $receivedData['region']['avgDailyIncomePopulation']; // Average daily income population
         $reportedCases = $receivedData['reportedCases'];
+        $periodType = $receivedData['periodType'];
+        $timeToElapse = $receivedData['timeToElapse'];
+        $population = $receivedData['population'];
+        $totalHospitalBeds = $receivedData['totalHospitalBeds'];
+        //Calculations
+		$days = get_num_days($periodType, $timeToElapse);
+		$factor = (int)($days/3);
 
         $impCurrentlyInfected = $reportedCases * 10; // Impact currently inected cases
         $severeImpCurrentlyInfected = $reportedCases * 50; // Severe Impact currently inected cases
-        $impInfByReqtime = $impCurrentlyInfected * 512; // Impact - infectionsByRequestedTime = $severeImpCurrentlyInfected *(2^9)
-        $severeImpInfByReqtime = $severeImpCurrentlyInfected * 512; // Severe Impact - infectionsByRequestedTime = $severeImpCurrentlyInfected * 512
+
+        $impInfByReqtime = (int)($impCurrentlyInfected * pow(2,$factor)); // Impact - infectionsByRequestedTime = $severeImpCurrentlyInfected *(2^9)
+        $severeImpInfByReqtime = (int)($severeImpCurrentlyInfected * pow(2,$factor)); // Severe Impact - infectionsByRequestedTime = $severeImpCurrentlyInfected * 512
         $impSevCasesByReqTime = (int)($impInfByReqtime * 0.15); //Impact  - severeCasesByRequestedTime
-        $servreImpSevCasesByReqTime = (int)($severeImpInfByReqtime * 0.15); //Sever Impact severeCasesByRequestedTime
+        $servreImpSevCasesByReqTime = (int)($severeImpInfByReqtime * 0.15); //Sever Impact - severeCasesByRequestedTime
 
 
         $impact['currentlyInfected'] = $impCurrentlyInfected;
@@ -40,7 +68,21 @@
         // Challenge 2
         $impact['severeCasesByRequestedTime'] = $impSevCasesByReqTime;
         $severeImpact['severeCasesByRequestedTime'] = $servreImpSevCasesByReqTime;
-        $availableBeds =  (int)($receivedData['totalHospitalBeds'] * .35);
+        $availableBeds =  (int)($receivedData['totalHospitalBeds'] * 0.35);
+
+        $impact['hospitalBedsByRequestedTime'] = (int)($availableBeds - $impSevCasesByReqTime);
+        $severeImpact['hospitalBedsByRequestedTime'] = (int)($availableBeds - $servreImpSevCasesByReqTime);
+
+        /// ***** Challenge 3 ****///
+        $impact['casesForICUByRequestedTime'] = (int)($impInfByReqtime * 0.05); // Impact  - Require ICU Care
+        $severeImpact['casesForICUByRequestedTime'] = (int)($severeImpInfByReqtime * 0.05); // Sever Impact - Require ICU Care
+        $impact['casesForVentilatorsByRequestedTime'] = (int)($impInfByReqtime * 0.02); // Impact  - Require ventilators
+        $severeImpact['casesForVentilatorsByRequestedTime'] = (int)($severeImpInfByReqtime * 0.02); // Sever Impact - Require ventilators
+        // Money lost
+        $impactMoneyLost = (int)(($impInfByReqtime * $averageIncomePop * $averageInconme)/$days);
+        $severeImpactMoneyLost = (int)(($severeImpInfByReqtime * $averageIncomePop * $averageInconme)/$days);
+        $impact['dollarsInFlight'] = $impactMoneyLost; // Impact  - Dollars in flight (Money lost)
+        $severeImpact['dollarsInFlight'] = $severeImpactMoneyLost; // Sever Impact - Dollars in flight (Money lost)
 
         $exportData = array(
             "data" => $receivedData, // the input data you got
